63534ddff4d2e583f7506ee8b9daceac530bcd61
Huynh Nguyen Phuc Vinh <phucvinh20112004@gmail.com>
2025-11-15 19:46:20 +0700

    fix code GameMapper

--- src/main/java/fit/iuh/dtos/GameDTO.java
+++ src/main/java/fit/iuh/dtos/GameDTO.java
@@ -14,7 +14,7 @@ import java.time.LocalDate;
 public class GameDTO {
     private Long id;
     private LocalDate releaseDate;
-    private GameBasicInforDTO gameBasicInfos;
+    private GameBasicInfoDto gameBasicInfos;
     private String categoryName;
 
 }
