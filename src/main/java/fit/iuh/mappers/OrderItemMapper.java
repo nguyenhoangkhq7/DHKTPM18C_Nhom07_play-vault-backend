1a61d28c526fc6ffc027921e634f8b64fbc667a8
deletebackspace <hoanpv076@gmail.com>
2025-11-19 02:54:23 +0700

    Thêm api của admindashboard

--- src/main/java/fit/iuh/mappers/OrderItemMapper.java
+++ src/main/java/fit/iuh/mappers/OrderItemMapper.java
@@ -6,30 +6,32 @@ import fit.iuh.models.OrderItem;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface OrderItemMapper {
 
     @Mapping(source = "order.id", target = "orderId")
-    @Mapping(source = "game.gameBasicInfos.id", target = "gameId")     
-    @Mapping(source = "game.gameBasicInfos.name", target = "gameTitle") 
-    @Mapping(source = "game.gameBasicInfos.thumbnail", target = "gameThumbnail")  
+    @Mapping(source = "game.gameBasicInfos.id", target = "gameId")
+    @Mapping(source = "game.gameBasicInfos.name", target = "gameTitle")
+    @Mapping(source = "game.gameBasicInfos.thumbnail", target = "gameThumbnail")
     @Mapping(source = "promotion.id", target = "promotionId")
-    @Mapping(target = "total", ignore = true)  
+    @Mapping(target = "total", ignore = true)
     OrderItemDto toDTO(OrderItem item);
 
     @Mapping(source = "orderId", target = "order.id")
     @Mapping(source = "gameId", target = "game.id")
     @Mapping(source = "promotionId", target = "promotion.id")
-    @Mapping(target = "total", ignore = true) // subtotal sẽ tự tính
+    @Mapping(target = "total", ignore = true)
+        // subtotal sẽ tự tính
     OrderItem toEntity(OrderItemDto dto);
 
-    // 3. Map từ OrderItem sang PurchasedGameResponse (MapStruct tự động gọi cái này khi map list ở trên)
-    // Do dữ liệu nằm sâu bên trong (game -> gameBasicInfos -> ...), ta dùng dấu chấm để truy cập
-    @Mapping(target = "gameId", source = "game.id")
-    @Mapping(target = "gameName", source = "game.gameBasicInfos.name")
-    @Mapping(target = "thumbnail", source = "game.gameBasicInfos.thumbnail")
-    @Mapping(target = "requiredAge", source = "game.gameBasicInfos.requiredAge")
-    @Mapping(target = "publisherName", source = "game.gameBasicInfos.publisher.studioName")
-    @Mapping(target = "categoryName", source = "game.gameBasicInfos.category.name")
-    PurchasedGameResponse toPurchasedGameResponse(OrderItem orderItem);
+    @Mapping(source = "order.id", target = "orderId")
+    @Mapping(source = "game.gameBasicInfos.id", target = "gameId")
+    @Mapping(source = "game.gameBasicInfos.name", target = "gameTitle")
+    @Mapping(source = "game.gameBasicInfos.thumbnail", target = "gameThumbnail")
+    @Mapping(source = "promotion.id", target = "promotionId")
+    @Mapping(target = "total", ignore = true)
+        // subtotal sẽ tự tính
+    List<OrderItemDto> toListDto(List<OrderItem> items);
 }
