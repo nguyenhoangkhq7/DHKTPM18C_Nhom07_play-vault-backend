d0031d62962a5328c006b7cfd9344e4ab835c4cd
HoangKhang <nguyenhoangkhq7@gmail.com>
2025-11-28 22:27:31 +0700

    sửa lỗi khi merge, giờ chạy được rồi

---
+++ src/main/java/fit/iuh/mappers/OrderItemMapper.java
@@ -0,0 +1,30 @@
+package fit.iuh.mappers;
+
+import fit.iuh.dtos.OrderItemDto;
+import fit.iuh.dtos.PurchasedGameResponse;
+import fit.iuh.models.OrderItem;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface OrderItemMapper {
+
+    @Mapping(source = "order.id", target = "orderId")
+    @Mapping(source = "game.gameBasicInfos.id", target = "gameId")
+    @Mapping(source = "game.gameBasicInfos.name", target = "gameTitle")
+    @Mapping(source = "game.gameBasicInfos.thumbnail", target = "gameThumbnail")
+    @Mapping(source = "promotion.id", target = "promotionId")
+    @Mapping(target = "total", ignore = true)
+    OrderItemDto toDTO(OrderItem item);
+
+    @Mapping(source = "orderId", target = "order.id")
+    @Mapping(source = "gameId", target = "game.id")
+    @Mapping(source = "promotionId", target = "promotion.id")
+    @Mapping(target = "total", ignore = true)
+        // subtotal sẽ tự tính
+    OrderItem toEntity(OrderItemDto dto);
+
+    List<OrderItemDto> toListDto(List<OrderItem> items);
+}
