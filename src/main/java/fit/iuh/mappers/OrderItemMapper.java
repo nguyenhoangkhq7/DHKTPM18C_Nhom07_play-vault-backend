749146a165cf4678a372056e183ab631f3ffcfe9
Huynh Nguyen Phuc Vinh <phucvinh20112004@gmail.com>
2025-11-29 21:58:57 +0700

    afetr merge 29/11

---
+++ src/main/java/fit/iuh/mappers/OrderItemMapper.java
@@ -0,0 +1,46 @@
+package fit.iuh.mappers;
+
+import fit.iuh.dtos.OrderItemDto;
+import fit.iuh.dtos.PurchasedGameResponse;
+import fit.iuh.models.OrderItem;
+import org.mapstruct.IterableMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface OrderItemMapper {
+
+    @Mapping(source = "order.id", target = "orderId")
+    @Mapping(source = "game.gameBasicInfos.id", target = "gameId")
+    @Mapping(source = "game.gameBasicInfos.name", target = "gameTitle")
+    @Mapping(source = "game.gameBasicInfos.thumbnail", target = "gameThumbnail")
+    @Mapping(source = "promotion.id", target = "promotionId")
+    @Mapping(target = "total", ignore = true)
+    OrderItemDto toDTO(OrderItem item);
+    // 2. Map List ← THÊM 2 DÒNG NÀY LÀ XONG!
+    @Named("toDTOList")
+    List<OrderItemDto> toDTOList(List<OrderItem> items);
+
+    List<PurchasedGameResponse> toPurchasedGameResponseList(List<OrderItem> orderItems);
+    // ← Dòng này nếu bạn dùng ở chỗ khác, không bắt buộc
+
+    @Mapping(source = "orderId", target = "order.id")
+    @Mapping(source = "gameId", target = "game.id")
+    @Mapping(source = "promotionId", target = "promotion.id")
+    @Mapping(target = "total", ignore = true)
+        // subtotal sẽ tự tính
+    OrderItem toEntity(OrderItemDto dto);
+
+    @Mapping(source = "order.id", target = "orderId")
+    @Mapping(source = "game.gameBasicInfos.id", target = "gameId")
+    @Mapping(source = "game.gameBasicInfos.name", target = "gameTitle")
+    @Mapping(source = "game.gameBasicInfos.thumbnail", target = "gameThumbnail")
+    @Mapping(source = "promotion.id", target = "promotionId")
+    @Mapping(target = "total", ignore = true)
+        // subtotal sẽ tự tính
+    @Named("toListDto")
+    List<OrderItemDto> toListDto(List<OrderItem> items);
+}
