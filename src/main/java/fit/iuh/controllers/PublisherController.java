121172ba76f1ee8950867c82175e33a7b2f39c2b
Le Nguyen Quang Vinh <quangvinh7137@gmail.com>
2025-12-01 21:07:57 +0700

    update backend publisher

--- src/main/java/fit/iuh/controllers/PublisherController.java
+++ src/main/java/fit/iuh/controllers/PublisherController.java
@@ -9,13 +9,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/publishers") // Đổi đường dẫn cho đúng nghiệp vụ
-@RequiredArgsConstructor
+@RequestMapping("/api/publishers")
+@AllArgsConstructor
 public class PublisherController {
 
     private final PublisherService publisherService;
@@ -66,5 +67,16 @@ public class PublisherController {
     public ResponseEntity<List<PublisherDto>> getPublisher(){
 
         return ResponseEntity.ok(publisherService.findAll());
+    }
+
+    @GetMapping("/by-username/{username}/profile")
+    public ResponseEntity<PublisherDto> getByUsername(@PathVariable String username) {
+        return ResponseEntity.ok(publisherService.getProfileByUsername(username));
+    }
+
+    @PutMapping("/{id}/profile")
+    public ResponseEntity<PublisherDto> update(@PathVariable Long id,
+                                               @RequestBody PublisherDto dto) {
+        return ResponseEntity.ok(publisherService.updateProfile(id, dto));
     }
 }
