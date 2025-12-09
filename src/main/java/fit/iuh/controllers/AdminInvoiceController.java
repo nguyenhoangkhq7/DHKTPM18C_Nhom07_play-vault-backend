88ad30267d38073168c1cbe31a87e32f9cb813f2
Le Nguyen Quang Vinh <quangvinh7137@gmail.com>
2025-12-09 23:01:13 +0700

    update invoice

---
+++ src/main/java/fit/iuh/controllers/AdminInvoiceController.java
@@ -0,0 +1,34 @@
+package fit.iuh.controllers;
+
+import fit.iuh.dtos.InvoiceTableDto;
+import fit.iuh.services.InvoiceService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
+
+@RestController
+@RequestMapping("/api/admin/invoices")
+@RequiredArgsConstructor
+public class AdminInvoiceController {
+
+    private final InvoiceService invoiceService;
+
+    // GET /api/admin/invoices?page=0&size=10&keyword=Nguyen
+    @GetMapping
+    public ResponseEntity<Page<InvoiceTableDto>> getAllInvoices(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(required = false) String keyword,
+            @RequestParam(required = false, defaultValue = "ALL") String status)
+            {
+        Pageable pageable = PageRequest.of(page, size);
+        Page<InvoiceTableDto> result = invoiceService.getInvoicesForAdmin(keyword, status,pageable);
+        return ResponseEntity.ok(result);
+    }
+
+
+}
