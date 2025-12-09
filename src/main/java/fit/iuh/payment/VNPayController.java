28bd822d3605cef014d7d25d817b81ecd8e343e6
HoangKhang <nguyenhoangkhq7@gmail.com>
2025-12-09 20:04:52 +0700

    đang làm phần vnpay

--- src/main/java/fit/iuh/payment/VNPayController.java
+++ src/main/java/fit/iuh/payment/VNPayController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("${spring.application.api-prefix}/payment")
+@RequestMapping("api/payment")
 @RequiredArgsConstructor
 public class VNPayController {
     private final VNPayService paymentService;
